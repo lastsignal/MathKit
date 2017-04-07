40e6c1647da583b4ffbf5bb4a484fdb0626b2cc1
lastsignal <bahram@lastsignal.com>
2017-04-06 23:55:29 -0500

    add more programs

--- app/services/program.js
+++ app/services/program.js
@@ -19,7 +19,27 @@
                 createOperand2Strategy: function() { return 8; },
                 createOperatorStrategy: function() { return '+'; }
             }
-        }
+        },
+        {
+            id: 'add-by-10',
+            title: 'Add by 10',
+            description: 'Add single-digit numbers to 10',
+            strategies: {
+                createOperand1Strategy: function() { return Math.floor(Math.random() * 10); },
+                createOperand2Strategy: function() { return 10; },
+                createOperatorStrategy: function() { return '+'; }
+            }
+        },
+        {
+            id: 'subtract-10-from-teens',
+            title: 'Subtract by 10',
+            description: 'Subtract 10 from teen numbers',
+            strategies: {
+                createOperand1Strategy: function() { return Math.floor(Math.random() * 10) + 10; },
+                createOperand2Strategy: function() { return 10; },
+                createOperatorStrategy: function() { return '-'; }
+            }
+        },
     ];
 
     return {
